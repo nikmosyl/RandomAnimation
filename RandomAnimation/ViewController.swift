9c90300232add5a9aef6476eb7f39cffc3a96524
2-39-swiftbook-joint-project <2.39.swiftbook.joint.project@gmail.com>
2024-01-26 18:32:56 +0400

    Initial Commit

---
+++ RandomAnimation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomAnimation
+//
+//  Created by nikita on 26.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
